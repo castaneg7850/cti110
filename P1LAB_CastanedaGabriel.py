7bdfd8ca33382bce3298c026f9f7906f57ec5ce2
castaneg7850 <castaneg7850@student.faytechcc.edu>
2025-04-02 13:23:28 -0400

    Add files via upload
    First submission

---
+++ P1LAB_CastanedaGabriel.py
@@ -0,0 +1,11 @@
+# Gabriel Castaneda
+# 4/2/2025
+# P1LAB
+# User info 
+
+# Get user input
+firstname = input("Enter your first name: ")
+lastname = input("Enter yout last name: ")
+
+# Print collected information
+print(f"Hello, {firstname} {lastname}! Welcome to CTI-110!")
