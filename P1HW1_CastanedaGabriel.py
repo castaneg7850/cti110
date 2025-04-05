cc68ad9a7fc9af2b4770a60e625948a2f12a4193
castaneg7850 <castaneg7850@student.faytechcc.edu>
2025-04-05 07:32:42 -0400

    Add files via upload

---
+++ P1HW1_CastanedaGabriel.py
@@ -0,0 +1,28 @@
+# Gabriel Castaneda
+# 4/05/2025
+# P1HW1
+# Calculating using input and print functions
+
+
+print("-----Calculating Exponents-----")
+print("\n")
+
+# Get base and exponents values from user
+base = int(input("Enter an integer as base vale: "))
+exponent = int(input("Enter an integer as exponent: "))
+
+# Calculate and display the result
+print("\n")
+print(base, "raise to the power of", exponent, "is", base**exponent, "!")
+
+print("-----Addition and subtraction-----")
+print("\n")
+
+# Get three integers from the user for calculating
+num1 = int(input("Enter a starting integer: "))
+num2 = int(input("Enter an integer to add: "))
+num3 = int(input("Enter an integer to subtract: "))
+
+# Calculate and display the result
+print("\n")
+print(num1, ("+"), num2, ("-"), num3, "is equal to", num1+num2-num3)
